5391b5331e3b8bcf083d1bda7ac01a4cee27fdd2
Ragglebonstew <145236701+Ragglebonstew@users.noreply.github.com>
2025-04-20 18:04:47 -0400

    removed flame spawner item

--- src/main/java/com/raggle/half_dream/common/registry/FaeItemRegistry.java
+++ src/main/java/com/raggle/half_dream/common/registry/FaeItemRegistry.java
@@ -4,8 +4,6 @@ import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 import com.raggle.half_dream.Faesied;
 import com.raggle.half_dream.common.item.BunnyPlushItem;
 import com.raggle.half_dream.common.item.DreamResin;
-import com.raggle.half_dream.common.item.FlameSpawner;
-
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -16,7 +14,6 @@ public class FaeItemRegistry {
 
 	public static final Item SHEEP_LAUREL = new Item(new QuiltItemSettings());
 	public static final DreamResin DREAM_RESIN = new DreamResin();
-	public static final Item FLAME_SPAWNER = new FlameSpawner();
 	public static final BunnyPlushItem BUNNY_PLUSH = new BunnyPlushItem(FaeBlockRegistry.BUNNY_PLUSH, new QuiltItemSettings());
 	
 	public static void init() {
@@ -26,7 +23,6 @@ public class FaeItemRegistry {
 		
 		Registry.register(Registries.ITEM, new Identifier(Faesied.MOD_ID, "interloper_portal_block"), new BlockItem(FaeBlockRegistry.INTERLOPER_PORTAL_BLOCK, new QuiltItemSettings()));
 		
-		Registry.register(Registries.ITEM, new Identifier(Faesied.MOD_ID, "flame_spawner"), FLAME_SPAWNER);
 		Registry.register(Registries.ITEM, new Identifier(Faesied.MOD_ID, "bunny_plush"), BUNNY_PLUSH);
 
 	}
