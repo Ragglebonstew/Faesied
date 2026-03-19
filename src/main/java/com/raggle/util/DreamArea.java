6c21f1bf49a699b06865d9adb79c7ad8afd2fe1f
Ragglebonstew <aprbark@gmail.com>
2026-03-19 15:51:00 -0400

    Added DreamArea attachable check

--- src/main/java/com/raggle/util/DreamArea.java
+++ src/main/java/com/raggle/util/DreamArea.java
@@ -12,15 +12,39 @@ public class DreamArea {
 	}
 	public boolean isContained(DreamArea area2) {
 		if(this.pos1.asLong() == area2.pos1.asLong() && this.pos2.asLong() == area2.pos2.asLong()) return true;
-		if(
-				this.pos1.getX() >= area2.pos1.getX() &&
-				this.pos1.getY() >= area2.pos1.getY() &&
-				this.pos1.getZ() >= area2.pos1.getZ() &&
-				this.pos2.getX() <= area2.pos2.getX() &&
-				this.pos2.getY() <= area2.pos2.getY() &&
-				this.pos2.getZ() <= area2.pos2.getZ()
-				)
-			return true;
+        return this.pos1.getX() >= area2.pos1.getX() &&
+                this.pos1.getY() >= area2.pos1.getY() &&
+                this.pos1.getZ() >= area2.pos1.getZ() &&
+                this.pos2.getX() <= area2.pos2.getX() &&
+                this.pos2.getY() <= area2.pos2.getY() &&
+                this.pos2.getZ() <= area2.pos2.getZ();
+    }
+
+	public boolean isAttachable(DreamArea area2){
+		boolean cornerx1 = this.pos1.getX() == area2.pos1.getX();
+		boolean cornery1 = this.pos1.getY() == area2.pos1.getY();
+		boolean cornerz1 = this.pos1.getZ() == area2.pos1.getZ();
+
+		boolean cornerx2 = this.pos2.getX() == area2.pos2.getX();
+		boolean cornery2 = this.pos2.getY() == area2.pos2.getY();
+		boolean cornerz2 = this.pos2.getZ() == area2.pos2.getZ();
+
+		if(cornerx1 && cornery1){
+			if(cornerx2 && cornery2) {
+				return Math.abs(this.pos1.getZ() - area2.pos1.getZ()) <= 1
+						|| Math.abs(this.pos2.getZ() - area2.pos2.getZ()) <= 1;
+			}
+		} else if (cornerx1 && cornerz1) {
+			if(cornerx2 && cornerz2) {
+				return Math.abs(this.pos1.getY() - area2.pos1.getY()) <= 1
+						|| Math.abs(this.pos2.getY() - area2.pos2.getY()) <= 1;
+			}
+		} else if (cornery1 && cornerz1) {
+			if(cornery2 && cornerz2) {
+				return Math.abs(this.pos1.getX() - area2.pos1.getX()) <= 1
+						|| Math.abs(this.pos2.getX() - area2.pos2.getX()) <= 1;
+			}
+		}
 		return false;
 	}
 }
